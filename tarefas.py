ffdff8016884d6e1c6bed716686c119b03154ff2
lu-nas <luanasilva.ss9497@gmail.com>
2025-05-27 12:26:45 -0300

    Ajuste tarefas como concluidas e menu

--- tarefas.py
+++ tarefas.py
@@ -10,9 +10,11 @@ def adicionar_tarefa():
 def listar_tarefas():
         if not tarefas:
             print("Nenhuma tarefa cadastrada.")
-        else:
-            for i, tarefa in enumerate(tarefas, start=1):
-                print(f"{i}. {tarefa['nome']} - prioridade: {tarefa['prioridade']}")
+            return
+        print("\n---Lista de Tarefas ---")
+        for i, t in enumerate(tarefas):
+            status = "✅" if t["concluida"] else "❌"
+            print(f"{i + 1}. {t['nome']} (prioridade: {t['prioridade']}) - {status}")
 
 def listar_prioridade_alta():
     tarefas_alta = [t for t in tarefas if t["prioridade"] == "alta"]
@@ -23,24 +25,37 @@ def listar_prioridade_alta():
         for i, tarefa in enumerate(tarefas_alta, start=1):
             print(f"{i}. {tarefa['nome']}")
 
-### Menu Principal
-while True:
-    print("\n--- MENU ---")
-    print("1. Adicionar tarefa")
-    print("2. Listar todas as tarefas")
-    print("3. Listar tarefas de prioridade ALTA")
-    print("4. Sair")
+def concluir_tarefa():
+    listar_tarefas()
 
-    opcao =input("Escolher uma opção: ")
+    try:
+        indice = int(input("\nDigite o numero da tarefa a concluir: ")) -1
+        if 0 <= indice <len(tarefas):
+            tarefas[indice]["concluida"] = True
+            print("Tarefa marcada como concluida!")
+        else: print("Número invalido.")
+    except ValueError:
+        print("Entrada invalida. Digite um número.")
 
-    if opcao == "1":
-        adicionar_tarefa()
-    elif opcao == "2":
-        listar_tarefas()
-    elif opcao == "3":
-        listar_prioridade_alta()
-    elif opcao == "4":
-        print("Encerrando o programa.")
-        break
-    else: 
-        print("Opção invalida . Tente noavamente.")
+   ### Menu Principal
+def menu():
+    while True:
+        print("\n--- MENU ---")
+        print("1. Adicionar tarefa")
+        print("2. Listar todas as tarefas")
+        print("3. mostrar tarefa como concluida ")
+        print("4. Sair")
+
+        opcao =input("Escolher uma opção: ")
+
+        if opcao == "1":
+            adicionar_tarefa()
+        elif opcao == "2":
+            listar_tarefas()
+        elif opcao == "3":
+            listar_prioridade_alta()
+        elif opcao == "4":
+            print("Encerrando o programa.")
+            break
+        else: 
+            print("Opção invalida.")
